cb2195e88871b96ed3f81ff7da9d1f19a46ac61d
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-03-11 14:10:10 +0100

    added unit-test for plugins/helpers.js

---
+++ test/plugins/helpers.js
@@ -0,0 +1,48 @@
+var test = require('ava');
+
+// Fake plugin-structure for wintersmith
+var env = {
+  helpers: {}
+};
+require('../../plugins/helpers')(env, function() {});
+
+/**
+ * creates a page-mock
+ * 
+ * @param  {string}  path  relative filepath
+ * @result {object}        page-mock
+ */
+function createPageMock(path) {
+  return {
+    filepath: {
+      relative: path
+    },
+    parent: {
+      parent: {
+        alerts: {},
+        progress: {}
+      }
+    }
+  };
+}
+
+test('createPatternTitle', function(t) {
+  var pageMock = createPageMock('/patterns/components/alerts/default.md')
+  
+  var title = env.helpers.createPatternTitle(pageMock);
+  var expected = 'Alerts';
+  
+  t.is(title, expected);
+});
+
+test('createSubcategoryUrl', function(t) {
+  var pagesMock = {
+    'default.md': createPageMock('/patterns/components/alerts/default.md'),
+    'link-color.md': createPageMock('patterns/components/alerts/link-color.md')
+  };
+  
+  var url = env.helpers.createSubcategoryUrl(pagesMock);
+  var expected = '/patterns/components/alerts/default.html';
+  
+  t.is(url, expected);
+});
