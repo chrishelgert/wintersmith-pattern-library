e62eff59941661660858f5988fa882dc13e3d127
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-03-11 13:29:35 +0100

    moved logic for title from jade to js-plugin

---
+++ plugins/helpers.js
@@ -0,0 +1,37 @@
+module.exports = function helpers(env, cb) {
+  /**
+   * capitalize the given word
+   * 
+   * @param  {String}  word
+   * @result {String}        capitalized word
+   */
+  function capitalize(word) {
+    return word.substr(0, 1).toUpperCase().concat(word.substr(1));
+  }
+  
+  /**
+   * creates the page-title for a pattern-template
+   * 
+   * @param  {Object}  page       current page
+   * @return {String}             pagetitle
+   */
+  function createPatternTitle(page) {
+    var parentCategory = page.parent.parent;
+    var currentPageUrl = page.filepath.relative;
+    
+    var title = '';
+    for(var prop in parentCategory) {
+      if(currentPageUrl.indexOf(prop) !== -1) {
+        title = prop;
+      }
+    }
+    
+    return capitalize(title);
+  }
+  
+  // add the functions to the environment, so we can use it
+  env.helpers.createPatternTitle = createPatternTitle;
+  
+  // tell the plugin manager we are done
+  cb();
+};
