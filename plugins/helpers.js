a6c0ff3ab6e7b1caaff9f9f3528dadb64aa2d652
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2016-03-11 14:09:37 +0100

    Lint helper.js

--- plugins/helpers.js
+++ plugins/helpers.js
@@ -1,49 +1,58 @@
+/* eslint no-param-reassign: [2, { "props": false }] */
+
 module.exports = function helpers(env, cb) {
   /**
-   * capitalize the given word
-   * 
-   * @param  {String}  word
-   * @result {String}        capitalized word
+   * Capitalize the given word
+   *
+   * @param {String} word
+   * @result {String} capitalized word
    */
   function capitalize(word) {
-    return word.substr(0, 1).toUpperCase().concat(word.substr(1));
+    return word
+      .substr(0, 1)
+      .toUpperCase()
+      .concat(word.substr(1));
   }
-  
+
   /**
-   * creates the page-title for a pattern-template
-   * 
-   * @param  {Object}  page       current page
-   * @return {String}             pagetitle
+   * Creates the page-title for a pattern-template
+   *
+   * @param {Object} page - current page
+   * @return {String} pagetitle
    */
   function createPatternTitle(page) {
     var parentCategory = page.parent.parent;
     var currentPageUrl = page.filepath.relative;
-    
     var title = '';
-    for(var prop in parentCategory) {
-      if(currentPageUrl.indexOf(prop) !== -1) {
+    var prop;
+
+    for (prop in parentCategory) {
+      if (currentPageUrl.indexOf(prop) !== -1) {
         title = prop;
       }
     }
-    
+
     return capitalize(title);
   }
-  
+
   /**
-   * creates a subcategory-url for the navigation
-   * 
-   * @param  {object}  pages  all pages for a category
-   * @return {string}         subcategory-url
+   * Creates a subcategory-url for the navigation
+   *
+   * @param {object} pages - all pages for a category
+   * @return {string subcategory-url
    */
   function createSubcategoryUrl(pages) {
     var key = Object.keys(pages)[0];
-    return pages[key].filepath.relative.replace('md', 'html');
+    return pages[key]
+      .filepath
+      .relative
+      .replace('md', 'html');
   }
-  
+
   // add the functions to the environment, so we can use it
   env.helpers.createPatternTitle = createPatternTitle;
   env.helpers.createSubcategoryUrl = createSubcategoryUrl;
-  
+
   // tell the plugin manager we are done
   cb();
 };
